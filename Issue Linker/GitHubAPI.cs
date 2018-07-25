fc52540c4346cd200c60879c0b301fc3e2bfcc1f
Andrei Maga <andrei.maga@student.upt.ro>
2018-07-25 20:32:48 +0300

    First Commit
    GitApi Works. Working on Visuals and auto repo detection.

---
+++ Issue Linker/GitHubAPI.cs
@@ -0,0 +1,53 @@
+﻿using Octokit;
+using System;
+using System.IO;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace Issue_Linker
+{
+    class GitHubAPI
+    {
+        //TODO: GET NAME OF THE GIT REPOSITORY
+        GitHubClient gitHubClient = new GitHubClient(new ProductHeaderValue("Issue-Linker-in-Visual-Studio"));
+
+        public async Task<Tuple<Issue, PullRequest>> GetObjectAsync(int number)
+        {
+
+            //string solutionName = Path.GetFileName(Process.GetCurrentProcess().MainModule.FileName);
+            string repositoryName = "Issue-Linker-in-Visual-Studio";
+            //TODO: Get the repository from git
+            var repo = await gitHubClient.Repository.Get("AndreiMaga", repositoryName);
+
+            var issue = await GetIssueAsync(repo.Id, number);
+            var pullRequest = await GetPullRequestAsync(repo.Id, number);
+            return new Tuple<Issue, PullRequest>(issue, pullRequest);
+        }
+
+        private async Task<Issue> GetIssueAsync(long id, int number)
+        {
+            try
+            {
+                return await gitHubClient.Issue.Get(id, number);
+            }
+            catch
+            {
+                return new Issue();
+            }
+
+        }
+
+        private async Task<PullRequest> GetPullRequestAsync(long id, int number)
+        {
+            try
+            {
+                return await gitHubClient.PullRequest.Get(id, number);
+            }
+            catch
+            {
+                return new PullRequest();
+            }
+        }
+    }
+}
---
+++ Issue Linker/TagDetector.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Windows.Media;
+using System.Text.RegularExpressions;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Formatting;
+using Microsoft.VisualStudio.Shell;
+using System.Threading.Tasks;
+using Octokit;
+
+namespace Issue_Linker
+{
+    /// <summary>
+    /// TagDetector places red boxes behind all the "a"s in the editor window
+    /// </summary>
+    internal sealed class TagDetector
+    {
+        /// <summary>
+        /// The layer of the adornment.
+        /// </summary>
+        private readonly IAdornmentLayer layer;
+
+        /// <summary>
+        /// Text view where the adornment is created.
+        /// </summary>
+        private readonly IWpfTextView view;
+
+        /// <summary>
+        /// Adornment brush.
+        /// </summary>
+        private readonly Brush brush;
+
+        /// <summary>
+        /// Adornment pen.
+        /// </summary>
+        private readonly Pen pen;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TagDetector"/> class.
+        /// </summary>
+        /// <param name="view">Text view to create the adornment for</param>
+        public TagDetector(IWpfTextView view)
+        {
+            if (view == null)
+            {
+                throw new ArgumentNullException("view");
+            }
+
+            this.layer = view.GetAdornmentLayer("TagDetector");
+
+            this.view = view;
+            this.view.LayoutChanged += this.OnLayoutChanged;
+
+            // Create the pen and brush to color the box behind the a's
+            this.brush = new SolidColorBrush(Color.FromArgb(0x20, 0x00, 0x00, 0xff));
+            this.brush.Freeze();
+
+            var penBrush = new SolidColorBrush(Colors.Red);
+            penBrush.Freeze();
+            this.pen = new Pen(penBrush, 0.5);
+            this.pen.Freeze();
+        }
+
+        /// <summary>
+        /// Handles whenever the text displayed in the view changes by adding the adornment to any reformatted lines
+        /// </summary>
+        /// <remarks><para>This event is raised whenever the rendered text displayed in the <see cref="ITextView"/> changes.</para>
+        /// <para>It is raised whenever the view does a layout (which happens when DisplayTextLineContainingBufferPosition is called or in response to text or classification changes).</para>
+        /// <para>It is also raised whenever the view scrolls horizontally or when its size changes.</para>
+        /// </remarks>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event arguments.</param>
+        internal void OnLayoutChanged(object sender, TextViewLayoutChangedEventArgs e)
+        {
+            foreach (ITextViewLine line in e.NewOrReformattedLines)
+            {
+                ThreadHelper.JoinableTaskFactory.RunAsync(async delegate
+                {
+                    await DetectionAsync(line);
+                    
+                });
+                
+            }
+        }
+        /// <summary>
+        /// Detects the pull request/issue by #number
+        /// </summary>
+        /// <param name="line">Line to add the adornments</param>
+        private async System.Threading.Tasks.Task DetectionAsync(ITextViewLine line)
+        {
+            IWpfTextViewLineCollection textViewLines = this.view.TextViewLines;
+
+            // Loop through each character, and place a box around any 'a'
+            //for (int charIndex = line.Start; charIndex < line.End; charIndex++)
+            var snap = this.view.TextSnapshot;
+            foreach (var l in this.view.TextSnapshot.Lines)
+            {
+                string text = l.GetText();
+                if (text.Contains("#"))
+                {
+                    // github regex
+                    Regex regex = new Regex(@"#\d+ ");
+                    Match match = regex.Match(text);
+                    if (match.Success)
+                    {
+                        int number = Int32.Parse(match.Value.Split('#')[1]);
+                        // call github api
+                        Console.WriteLine("Calling the GitHub API, it might take a second");
+                        Tuple<Issue, PullRequest> result = await new GitHubAPI().GetObjectAsync(number);
+                        //Do graphics with the 
+                        
+
+                        return;
+                    }
+
+                    // jira regex
+                    regex = new Regex(@"#\w+-\d+ ");
+                    match = regex.Match(text);
+                    if (match.Success)
+                    {
+                        // call jira api
+
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
